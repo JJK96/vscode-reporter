c067cb1fddcc204e167716e47f76c8d8e6a3c6cd
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-06-29 09:42:39 +0200

    Add completion provider for location

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as cp from 'child_process';
-import * as process from 'process'
+import * as process from 'process';
+const Cache:any = require('vscode-cache');
 
 function get_dir(p: string) {
 	let dir = p
@@ -36,6 +37,15 @@ async function getValuePrompt(choices: Array<string>) {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let cache = new Cache(context)
+
+	function get_locations(filename: string) {
+		// Using a single cache entry for all files. If this gives problems or unexpected behaviour it should be changed
+		if (cache.has('locations')) return cache.get('locations')
+		let locations = cp.execSync(`cd "${get_dir(filename)}"; reporter locations`).toString().split("\n")	
+		cache.put('locations', locations, 5) //Expire after a minute
+		return locations
+	}
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -61,12 +71,20 @@ export function activate(context: vscode.ExtensionContext) {
 	let create_evidence = vscode.commands.registerCommand('vscode-reporter.create_evidence', async (file) => {
 		if (!file) return;
 		let dir = get_dir(file.path)
-		let locations = cp.execSync(`cd "${dir}"; reporter locations`).toString().split("\n")
+		let locations = get_locations(file.path)
 		let location = await getValuePrompt(locations)
 		let command = `cd "${dir}"; reporter create-evidence -l "${location}"`
 		cp.exec(command)
 	})
 	context.subscriptions.push(create_evidence);
+
+	let location_completer = vscode.languages.registerCompletionItemProvider('textile', {
+		provideCompletionItems(document, position, token, context) {
+			let locations = get_locations(document.fileName)
+			return locations.map((l:string) => new vscode.CompletionItem(l))
+		}
+	})
+	context.subscriptions.push(location_completer)
 	
 	//Run latex workshop build also when dradis/issue files are edited
 	var latexWorkshop = vscode.extensions.getExtension( 'James-Yu.latex-workshop' );
