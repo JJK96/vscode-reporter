fc028649d5ca9a16ebd7be3990e55aa99362ffde
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2022-06-29 13:16:46 +0200

    Only add context menu if extension is active
    Extension is activated when report.tex is found

--- src/extension.ts
+++ src/extension.ts
@@ -50,6 +50,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-reporter" is now active!');
+	vscode.commands.executeCommand('setContext', 'vscode-reporter.active', true)
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
